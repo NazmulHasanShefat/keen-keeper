75b1336c79d3f1b8352b56db1562d80642625c89
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-04-15 16:34:57 +0600

    fix time line page css bugs

--- src/app/timeline/TimeLineList.jsx
+++ src/app/timeline/TimeLineList.jsx
@@ -22,8 +22,8 @@ const TimeLineList = ({ selectedValue }) => {
   return ConnectionList.length === 0 || !ConnectionList ? (
     <>
       <div className="flex flex-col items-center">
-        <h1 className="text-7xl font-bold text-center">404 Not found</h1>
-        <p className="text-[18px] text-gray-500 my-2 text-center">Please add some timeline</p>
+        <h1 className="text-[30px] md:text-[48px] lg:text-[48px] font-bold text-center">404 Not found</h1>
+        <p className="text-[18px] text-gray-500 my-2 text-center">Please communicate with your friends</p>
         <Link
           href={"/"}
           className="py-2 rounded-xl hover:scale-105 transition-[scale] duration-200 cursor-pointer px-4 bg-green-700 text-white text-base"
@@ -39,7 +39,7 @@ const TimeLineList = ({ selectedValue }) => {
           key={index}
           className="list-row grid grid-cols-12 border border-gray-200 my-2"
         >
-          <div className="flex items-center gap-3 col-span-10">
+          <div className="flex items-center gap-3 col-span-8 lg:col-span-10 md:col-span-10">
             {connectionTimeline.communicationType === "Call" ?
             <PhoneCall size={30}/>:
             connectionTimeline.communicationType === "Message" ?
@@ -59,7 +59,7 @@ const TimeLineList = ({ selectedValue }) => {
             </div>
           </div>
 
-          <div className="flex justify-end items-center col-span-2">
+          <div className="flex justify-end items-center col-span-4 lg:col-span-2 md:col-span-2">
             <p className="text-gray-400"> {connectionTimeline.communicationTime} </p>
           </div>
         </li>
